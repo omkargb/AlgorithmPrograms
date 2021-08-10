ef877690ff629d60c4c591cd7c863a27570c9336
omkargb <omkargb107@gmail.com>
2021-08-10 20:10:18 +0530

    [omkargb] Add : Searching for a text in text file using binary search program implemented.

---
+++ AlgorithmPrograms/BinarySearchWord.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AlgorithmPrograms
+{ 
+    class BinarySearchWord
+    {
+        public static void BinarySearch(String[] wordList, String searchTerm)
+        {
+            int arrayLength = wordList.Length;
+            int left = 0,  right = arrayLength - 1;
+            while (left <= right)
+            {
+                int mid = left + ( right - left) / 2;
+                int result = searchTerm.CompareTo(wordList[mid]);
+
+                // Check if term is present at mid
+                if (result == 0)
+                {
+                    Console.WriteLine(" Search String present..");
+                    break;
+                }
+                // If term greater, ignore left half
+                else if (result > 0)
+                {
+                    left = mid + 1;
+                }
+                // If term is smaller, ignore right half
+                else if (result < 0)
+                {
+                    right = mid - 1;
+                }
+                if (left > right)
+                {
+                    Console.WriteLine(" Search String not present..");
+                    break;
+                }
+            }
+
+        }
+
+        public void inputArray()
+        {
+            string wordList = File.ReadAllText(@"G:\BRIDGELABZ\AlgorithmProgramsDotnet\AlgorithmPrograms\InputFile.txt");
+            Console.WriteLine(" Input file contains : ");
+            //String[] words = { "My", "Name", "is", "Omkar" };
+            string[] words = wordList.Split(',');
+            foreach (string data in words)
+            {
+                Console.Write(" "+data);
+            }
+            Array.Sort(words);
+            Console.Write("\n Enter a search term : ");
+            String searchTerm = Console.ReadLine();
+            BinarySearch(words, searchTerm);
+        }
+    }
+}
---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to algorithms based programs..");
+
+            Console.WriteLine(" Here is the list of programs : " +
+                "\n 1. Binary Search" +
+                "\n 2. Insertion sort" +
+                "\n 3. Bubble sort" +
+                "\n 4. Merge sort" +
+                "\n 5. Anagram Detection" +
+                "\n 6. Prime numbers in 1 to 1000" +
+                "\n 7. Find Anagram and palindrome prime numbers." +
+                "\n 8. Guess the number."+
+                "\n 0. Exit");
+
+            int choice=99;
+            while (choice != 0)
+            {
+
+                Console.Write("\n Enter your choice number : ");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\n Binary Searching for a word in array..");
+                        BinarySearchWord bs = new BinarySearchWord();
+                        bs.inputArray();
+                        break;
+                    default:
+                        Console.WriteLine(" Invalid choice number..");
+                        break;
+                }
+            }
+        }
+
+    }
+}

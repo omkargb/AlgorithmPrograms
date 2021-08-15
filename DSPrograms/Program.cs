7e9f259e97ec06abe47426c5c28e2488ed31695d
omkargb <omkargb107@gmail.com>
2021-08-15 22:25:47 +0530

    [omkargb] Add : UnOrdered linked list program - search element, delete or add to list- implemeted.

---
+++ DSPrograms/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace DSPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Implementation of Data Structure Programs ");
+            Console.WriteLine("\n Here is the list of programs : ");
+            Console.WriteLine(" 1. UnOrdered List - search , add and delete element \t \n");
+
+            Console.Write(" Please provide option number : ");
+            int option = int.Parse(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    string filePath = @"G:\BRIDGELABZ\AlgorithmProgramsDotnet\DSPrograms\UOListInput.txt";
+                    UnOrderedList<string> unolist = new UnOrderedList<string>();
+                    //reading text from file
+                    string text = File.ReadAllText(filePath);
+                    Console.WriteLine(" Input file contents → " + text);
+
+                    string[] textArray = text.Split(" ");
+                    for (int i = 0; i < textArray.Length; i++)
+                    {
+                        unolist.AddLast(textArray[i]);
+                    }
+
+                    Console.Write("\n\n Enter the word to search : ");
+                    string word = Console.ReadLine();
+                    int found = unolist.SearchNode(word);
+                    if (found == 1)
+                    {
+                        unolist.DeleteNode(word);
+                    }
+                    else
+                    {
+                        unolist.AddLast(word);
+                    }
+                    string newText = unolist.Process();
+                    File.WriteAllText(filePath, newText);
+                    Console.WriteLine(" After Updating - Input file contents → " +newText);
+                    break;
+
+                default:
+                    Console.WriteLine(" Invalid choice..");
+                    break;
+            }
+        }
+    }
+}
---
+++ DSPrograms/UnOrderedList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSPrograms
+{
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> Next;
+        public Node(T value)
+        {
+            this.value = value;
+            this.Next = null;
+        }
+    }
+    public class UnOrderedList<T>
+    {
+        private Node<T> head;
+
+        public void AddLast(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else 
+            {
+                Node<T> temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+        }
+
+        public void DeleteNode(T value)
+        {
+            if (head == null)
+            {
+                Console.WriteLine(" No element to delete.. ");
+            }
+            else if (head.value.Equals(value))
+            {
+                head = head.Next;
+            }
+            else
+            {
+                Node<T> prev = head, temp = head;
+                while (temp != null && !(temp.value.Equals(value)))
+                {
+                    prev = temp;
+                    temp = temp.Next;
+                }
+                if (temp.value.Equals(value))
+                {
+                    prev.Next = temp.Next;
+                    Console.WriteLine(" Element : {0} is Removed..", value); ;
+                    temp.Next = null;
+                }
+            }
+        }
+
+        //method to find/search for the element
+        public int SearchNode(T value)
+        {
+            if (head == null)
+            {
+                return 0;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.Next != null && !(temp.value.Equals(value)))
+                {
+                    temp = temp.Next;
+                }
+                if (temp.value.Equals(value))
+                {
+                    return 1;
+                }
+                return 0;
+            }
+        }
+
+        //method to display the elements 
+        public string Process()
+        {
+            Node<T> temp = head;
+            string data = "";
+            if (head == null)
+            {
+                Console.WriteLine(" List is empty..");
+            }
+            else
+            {
+                while (temp.Next != null)
+                {
+                    data = data + temp.value + " ";
+                    temp = temp.Next;
+                }
+                data = data + temp.value;
+            }
+            return data;
+        }
+    }
+}
